0a5054ebbbab1137be6ca4acc55fca1e9050dc94
SalTor <storcivia@capsule.com>
2021-04-07 18:17:10 -0400

    vim: telescope use fzy native sorting

--- nvim/plug-config/telescope.lua
+++ nvim/plug-config/telescope.lua
@@ -3,12 +3,30 @@ local actions = require('telescope.actions')
 -------------------
 require('telescope').setup{
     defaults = {
-        prompt_position = "top",
-        sorting_strategy = "ascending",
+        file_sorter = require('telescope.sorters').get_fzy_sorter,
+        color_devicons = true,
+        prompt_position = 'top',
+        sorting_strategy = 'ascending',
+
         mappings = {
             i = {
-                ["<ESC>"] = actions.close,
+                ['<ESC>'] = actions.close,
+                ['<C-u>'] = false
             }
         },
-    }
+
+        file_previewer = require('telescope.previewers').vim_buffer_cat.new,
+        grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
+        qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+    },
+    extensions = {
+        fzy_native = {
+            override_generic_sorter = false,
+            override_file_sorter = true,
+        }
+    },
+}
+
+require('nvim-web-devicons').setup{
+    default = true
 }
--- nvim/vim-plug/plugins.vim
+++ nvim/vim-plug/plugins.vim
@@ -56,6 +56,7 @@ call plug#begin('~/.config/nvim/autoload/plugged')
     Plug 'nvim-lua/popup.nvim'
     Plug 'nvim-lua/plenary.nvim'
     Plug 'nvim-telescope/telescope.nvim'
+    Plug 'nvim-telescope/telescope-fzy-native.nvim'
 
     Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
 call plug#end()
