2300398311a8580abe12b6e4b4a55b43174d97c9
SalTor <storcivia@capsule.com>
2021-04-15 18:45:42 -0400

    vim: Tweak startify config

--- nvim/lua/config/startify.lua
+++ nvim/lua/config/startify.lua
@@ -5,18 +5,24 @@ vim.g.startify_session_autoload = 1
 vim.g.startify_change_to_vcs_root = 1
 vim.g.startify_session_persistence = 1
 vim.g.startify_enable_special = 0
+vim.cmd [[ let g:startify_session_dir = '~/.config/nvim/session' ]]
 
--- vim.cmd [[
-    -- let g:startify_session_dir = '~/.config/nvim/session'
---
-    -- let g:startify_lists = [
-            -- \ { 'type': 'dir',       'header': ['        MRU '. getcwd()] },
-            -- \ { 'type': 'sessions',  'header': ['        Sessions']  },
-            -- \ { 'type': 'bookmarks', 'header': ['        Bookmarks'] },
-            -- \ ]
---
-    -- let g:startify_bookmarks = [
-            -- \ { 'i': '~/.config/nvim/init.vim' },
-            -- \ { 'z': '~/.zshrc' },
-            -- \ ]
--- ]]
+vim.g.startify_lists = {
+    {
+        type = 'dir',
+        header = { '        MRU ' .. vim.fn.getcwd() }
+    },
+    {
+        type = 'sessions',
+        header = { '        Sessions' }
+    },
+    {
+        type = 'bookmarks',
+        header = { '        Bookmarks' }
+    }
+}
+
+vim.g.startify_bookmarks = {
+    { i = '~/.config/nvim/init.vim' },
+    { z = '~/.zshrc' }
+}
