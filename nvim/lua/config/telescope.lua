5d31d3636be1c728291d8f1623398508a0383990
SalTor <storcivia@capsule.com>
2021-04-18 16:34:20 -0400

    vim: update telescope settings

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -8,10 +8,33 @@ require'telescope'.setup {
         prompt_position = 'top',
         sorting_strategy = 'ascending',
 
+        find_command = {'rg', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case'},
+        set_env = {['COLORTERM'] = 'truecolor'}, -- default = nil
         mappings = {
             i = {
-                ['<ESC>'] = actions.close,
-                ['<C-u>'] = false
+                ['<C-u>'] = false,
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
+                ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+                -- To disable a keymap, put [map] = false
+                -- So, to not map "<C-n>", just put
+                -- ["<c-x>"] = false,
+                ["<esc>"] = actions.close,
+
+                -- Otherwise, just set the mapping to the function that you want it to be.
+                -- ["<C-i>"] = actions.select_horizontal,
+
+                -- Add up multiple actions
+                ["<CR>"] = actions.select_default + actions.center
+
+                -- You can perform as many actions in a row as you like
+                -- ["<CR>"] = actions.select_default + actions.center + my_cool_custom_action,
+            },
+            n = {
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
+                ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+                -- ["<C-i>"] = my_cool_custom_action,
             }
         },
 
