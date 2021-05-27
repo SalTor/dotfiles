ccbcf2171221ad2f3e89f589037d2aa24c5eefbd
SalTor <storcivia@capsule.com>
2021-05-27 14:40:17 -0400

    emacs: use organge for evil goggles

--- emacs/init.el
+++ emacs/init.el
@@ -69,6 +69,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(evil-goggles-yank-face ((t (:inherit evil-goggles-default-face :background "DarkOrange1"))))
  '(flycheck-check-syntax-automatically '(save idle-change new-line mode-enabled))
  '(flycheck-locate-config-file-functions '(flycheck-locate-config-file-ancestor-directories flycheck-locate-config-file-by-path))
  '(flycheck-posframe-background-face ((t nil)))
