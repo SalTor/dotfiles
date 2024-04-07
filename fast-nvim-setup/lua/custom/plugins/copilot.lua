bbbaba1dc1bf05f21e80ad512d590a144050ecbd
SalTor <sal@saltor.nyc>
2024-04-07 17:38:19 -0400

    disable copilot (for now)

--- fast-nvim-setup/lua/custom/plugins/copilot.lua
+++ fast-nvim-setup/lua/custom/plugins/copilot.lua
@@ -1,53 +1,53 @@
 return {
-  "zbirenbaum/copilot.lua",
+  -- "zbirenbaum/copilot.lua",
   -- cmd = "Copilot",
   -- event = "InsertEnter",
-  config = function()
-    require('copilot').setup({
-      suggestion = { enabled = false },
-      panel = { enabled = false },
-      -- panel = {
-      --   enabled = true,
-      --   auto_refresh = false,
-      --   keymap = {
-      --     jump_prev = "[[",
-      --     jump_next = "]]",
-      --     accept = "<CR>",
-      --     refresh = "gr",
-      --     open = "<M-CR>"
-      --   },
-      --   layout = {
-      --     position = "bottom", -- | top | left | right
-      --     ratio = 0.4
-      --   },
-      -- },
-      -- suggestion = {
-      --   enabled = false,
-      --   auto_trigger = true,
-      --   debounce = 75,
-      --   keymap = {
-      --     accept = "¬",
-      --     accept_word = false,
-      --     accept_line = false,
-      --     next = "˜",
-      --     prev = "π",
-      --     dismiss = "≈",
-      --   },
-      -- },
-      -- filetypes = {
-      --   yaml = false,
-      --   markdown = false,
-      --   help = false,
-      --   gitcommit = false,
-      --   gitrebase = false,
-      --   hgcommit = false,
-      --   svn = false,
-      --   cvs = false,
-      --   typescript = true,
-      --   ["."] = false,
-      -- },
-      -- copilot_node_command = 'node', -- Node.js version must be > 18.x
-      -- server_opts_overrides = {},
-    })
-  end
+  -- config = function()
+  --   require('copilot').setup({
+  --     suggestion = { enabled = false },
+  --     panel = { enabled = false },
+  --     -- panel = {
+  --     --   enabled = true,
+  --     --   auto_refresh = false,
+  --     --   keymap = {
+  --     --     jump_prev = "[[",
+  --     --     jump_next = "]]",
+  --     --     accept = "<CR>",
+  --     --     refresh = "gr",
+  --     --     open = "<M-CR>"
+  --     --   },
+  --     --   layout = {
+  --     --     position = "bottom", -- | top | left | right
+  --     --     ratio = 0.4
+  --     --   },
+  --     -- },
+  --     -- suggestion = {
+  --     --   enabled = false,
+  --     --   auto_trigger = true,
+  --     --   debounce = 75,
+  --     --   keymap = {
+  --     --     accept = "¬",
+  --     --     accept_word = false,
+  --     --     accept_line = false,
+  --     --     next = "˜",
+  --     --     prev = "π",
+  --     --     dismiss = "≈",
+  --     --   },
+  --     -- },
+  --     -- filetypes = {
+  --     --   yaml = false,
+  --     --   markdown = false,
+  --     --   help = false,
+  --     --   gitcommit = false,
+  --     --   gitrebase = false,
+  --     --   hgcommit = false,
+  --     --   svn = false,
+  --     --   cvs = false,
+  --     --   typescript = true,
+  --     --   ["."] = false,
+  --     -- },
+  --     -- copilot_node_command = 'node', -- Node.js version must be > 18.x
+  --     -- server_opts_overrides = {},
+  --   })
+  -- end
 }
