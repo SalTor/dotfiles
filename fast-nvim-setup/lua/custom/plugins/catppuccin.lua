254443afedb891c96e943a6bfee7fe832bcd26fa
SalTor <sal@saltor.nyc>
2024-04-07 17:38:36 -0400

    update catpuccin settings

--- fast-nvim-setup/lua/custom/plugins/catppuccin.lua
+++ fast-nvim-setup/lua/custom/plugins/catppuccin.lua
@@ -1,11 +1,8 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
+  'catppuccin/nvim',
+  name = 'catppuccin',
   priority = 1000,
   config = function()
-    -- setup must be called before loading
-    vim.cmd.colorscheme "catppuccin-macchiato"
-
-    require("catppuccin").setup()
-  end
+    vim.cmd.colorscheme 'catppuccin-frappe'
+  end,
 }
