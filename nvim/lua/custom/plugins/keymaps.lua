48deaf46fbbdca54e807091d45a55a87806729b9
Sal Torcivia <storcivia@fastgrowingtrees.com>
2025-01-09 11:09:24 -0500

    re-define harpoon bindings for qwerty and colemak

--- nvim/lua/custom/plugins/keymaps.lua
+++ nvim/lua/custom/plugins/keymaps.lua
@@ -38,21 +38,20 @@ return {
     'ThePrimeagen/harpoon',
     config = function()
       local nmap = require('saltor').nmap
-      nmap('<leader>ha', "<cmd>lua require('harpoon.mark').add_file()<CR>")
-      nmap('<leader>hl', "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>")
+      nmap('<leader>na', "<cmd>lua require('harpoon.mark').add_file()<CR>")
       nmap('<leader>nm', "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>")
 
-      nmap('<leader>h1', "<cmd>lua require('harpoon.ui').nav_file(1)<CR>")
+      -- Colemak bindings
       nmap('<leader>nn', "<cmd>lua require('harpoon.ui').nav_file(1)<CR>")
-
-      nmap('<leader>h2', "<cmd>lua require('harpoon.ui').nav_file(2)<CR>")
       nmap('<leader>ne', "<cmd>lua require('harpoon.ui').nav_file(2)<CR>")
-
-      nmap('<leader>h3', "<cmd>lua require('harpoon.ui').nav_file(3)<CR>")
       nmap('<leader>ni', "<cmd>lua require('harpoon.ui').nav_file(3)<CR>")
-
-      nmap('<leader>h4', "<cmd>lua require('harpoon.ui').nav_file(4)<CR>")
       nmap('<leader>no', "<cmd>lua require('harpoon.ui').nav_file(4)<CR>")
+
+      -- Qwerty bindings
+      nmap('<leader>n1', "<cmd>lua require('harpoon.ui').nav_file(1)<CR>")
+      nmap('<leader>n2', "<cmd>lua require('harpoon.ui').nav_file(2)<CR>")
+      nmap('<leader>n3', "<cmd>lua require('harpoon.ui').nav_file(3)<CR>")
+      nmap('<leader>n4', "<cmd>lua require('harpoon.ui').nav_file(4)<CR>")
     end,
   },
   {
