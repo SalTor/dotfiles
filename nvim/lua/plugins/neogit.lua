706c1c1036940b066ce2ca4e243fc4ee24433563
saltor <storcivia@fastgrowingtrees.com>
2025-08-31 16:50:47 -0400

    Comment out neogit as I explore jj

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -1,12 +1,12 @@
 return {
-  'NeogitOrg/neogit',
-  dependencies = {
-    'nvim-lua/plenary.nvim', -- required
-    'nvim-telescope/telescope.nvim', -- optional
-    -- 'sindrets/diffview.nvim', -- optional
-  },
-  config = true,
-  init = function()
-    require('saltor').nmap('<leader>gs', require('neogit').open)
-  end,
+  -- 'NeogitOrg/neogit',
+  -- dependencies = {
+  --   'nvim-lua/plenary.nvim', -- required
+  --   'nvim-telescope/telescope.nvim', -- optional
+  --   -- 'sindrets/diffview.nvim', -- optional
+  -- },
+  -- config = true,
+  -- init = function()
+  --   require('saltor').nmap('<leader>gs', require('neogit').open)
+  -- end,
 }
