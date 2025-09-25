71aead273af5e3ab67ed53cd044ad43b0639ed08
saltor <storcivia@fastgrowingtrees.com>
2025-09-25 09:56:32 -0400

    Add blink source for dadbod to get auto complete on table names and column names

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -44,6 +44,13 @@ return {
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
+      per_filetype = {
+        sql = { 'snippets', 'dadbod', 'buffer' },
+      },
+      -- optionally disable cmdline completions
+      providers = {
+        dadbod = { name = 'Dadbod', module = 'vim_dadbod_completion.blink' },
+      },
     },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
