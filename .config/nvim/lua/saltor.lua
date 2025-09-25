d31c81dd31e88d960e2ecdc15456753b68a5222a
saltor <storcivia@fastgrowingtrees.com>
2025-09-25 09:56:32 -0400

    Replace lualine's branch section (for git) with my own jujutsu one

--- .config/nvim/lua/saltor.lua
+++ .config/nvim/lua/saltor.lua
@@ -48,4 +48,19 @@ function M.is_inside_git_repo()
   return false
 end
 
+function M.dump(o)
+  if type(o) == 'table' then
+    local s = '{ '
+    for k, v in pairs(o) do
+      if type(k) ~= 'number' then
+        k = '"' .. k .. '"'
+      end
+      s = s .. '[' .. k .. '] = ' .. M.dump(v) .. ','
+    end
+    return s .. '} '
+  else
+    return tostring(o)
+  end
+end
+
 return M
