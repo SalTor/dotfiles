3a9384512e6bb7ad0f8f277b30ba48c8092e4ee4
saltor <storcivia@fastgrowingtrees.com>
2025-10-28 09:54:05 -0400

    Add JJDiffQuickfix for putting changed files in quickfix list

--- .config/nvim/lua/config/jujutsu.lua
+++ .config/nvim/lua/config/jujutsu.lua
@@ -1,2 +1,3 @@
 require('config/jujutsu/status').config()
 require('config/jujutsu/conflicts-to-qf').config()
+require 'config/jujutsu/diff-to-qf'
---
+++ .config/nvim/lua/config/jujutsu/diff-to-qf.lua
@@ -0,0 +1,24 @@
+vim.api.nvim_create_user_command('JJDiffQuickfix', function()
+  -- Run jj diff --summary and capture output
+  local output = vim.fn.systemlist 'jj diff --summary'
+
+  local files = {}
+  for _, line in ipairs(output) do
+    -- Skip lines starting with "D "
+    if not line:match '^D ' then
+      local path = line:match '%S+%s+(.*)'
+      if path then
+        table.insert(files, { filename = path })
+      end
+    end
+  end
+
+  if #files == 0 then
+    vim.notify('No modified files found', vim.log.levels.INFO)
+    return
+  end
+
+  -- Populate the quickfix list
+  vim.fn.setqflist({}, 'r', { title = 'jj diff --summary', items = files })
+  vim.cmd 'copen'
+end, {})
