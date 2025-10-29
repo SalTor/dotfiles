bdda7c3b51b6eb51be9fa01d4b9027c4b462ac14
saltor <storcivia@fastgrowingtrees.com>
2025-10-29 11:39:17 -0400

    Remove unused lsp config

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,7 +1,5 @@
 local nmap = require('saltor').nmap
 
-local capabilities = require('blink.cmp').get_lsp_capabilities()
-
 vim.diagnostic.config {
   virtual_text = true,
   virtual_lines = false,
@@ -29,11 +27,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
     -- Lesser used LSP functionality
     nmap('gD', vim.lsp.buf.declaration, 'LSP: [G]oto [D]eclaration')
-    -- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, 'LSP: [W]orkspace [A]dd Folder')
-    -- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, 'LSP: [W]orkspace [R]emove Folder')
-    -- nmap('<leader>wl', function()
-    --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    -- end, 'LSP: [W]orkspace [L]ist Folders')
 
     -- Create a command `:Format` local to the LSP buffer
     vim.api.nvim_buf_create_user_command(event.buf, 'Format', function(_)
@@ -69,119 +62,3 @@ vim.lsp.config('tailwindcss', {
     },
   },
 })
-
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
---
---  If you want to override the default filetypes that your language server will attach to you can
---  define the property 'filetypes' to the map in question.
--- local servers = {
---   -- clangd = {},
---   -- gopls = {},
---   -- pyright = {},
---   -- rust_analyzer = {},
---   -- tsserver = {},
---   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
---   -- emmet_ls = {
---   --   filetypes = { 'html', 'typescriptreact' },
---   -- },
---
---   lua_ls = {
---     Lua = {
---       workspace = { checkThirdParty = false },
---       telemetry = { enable = false },
---     },
---   },
---
--- tailwindcss = {
---   tailwindCSS = {
---     classFunctions = { 'cva', 'tv', 'cn' },
---     experimental = {
---       classRegex = {
---         -- { '(["\'`][^"\'`]*.*?["\'`])', '["\'`]([^"\'`]*).*?["\'`]' }, -- e.g., all strings
---         -- { 'cn\\(([^)]*)\\)', '["\'`]([^"\'`]+)["\'`]' }, -- e.g., cn("...")
---         -- { 'tv\\(([^)]*)\\)', '["\'`]([^"\'`]*).*?["\'`]' }, -- e.g., tv("...")
---         { 'Styles \\=([^;]*);', "'([^']*)'" }, -- e.g. const nameStyles = '...'
---         { 'Styles \\=([^;]*);', '"([^"]*)"' }, -- e.g. const nameStyles = "..."
---         { 'Styles \\=([^;]*);', '\\`([^\\`]*)\\`' }, -- e.g. const nameStyles = `...`
---         -- { 'lassName\\=([^;]*);', '"([^"]*)"' }, -- e.g., <MyDiv nameClassName="..." />
---       },
---     },
---   },
--- },
--- }
-
-local thing = {
-  function(server_name)
-    local util = require 'lspconfig/util'
-
-    if server_name == 'tsserver' then
-      local function organize_imports()
-        local params = {
-          command = '_typescript.organizeImports',
-          arguments = { vim.api.nvim_buf_get_name(0) },
-          title = '',
-        }
-        vim.lsp.buf.execute_command(params)
-      end
-
-      require('lspconfig').tsserver.setup {
-        handlers = handlers,
-        capabilities = capabilities,
-        on_attach = on_attach,
-        commands = {
-          OrganizeImports = {
-            organize_imports,
-            description = 'Organize Imports',
-          },
-        },
-      }
-    elseif server_name == 'rust_analyzer' then
-      require('lspconfig').rust_analyzer.setup {
-        handlers = handlers,
-        on_attach = on_attach,
-        capabilities = capabilities,
-        filetypes = { 'rust' },
-        root_dir = util.root_pattern 'Cargo.toml',
-        settings = {
-          ['rust_analyzer'] = {
-            cargo = {
-              allFeatures = true,
-            },
-          },
-        },
-      }
-    elseif server_name == 'pylsp' then
-      require('lspconfig').pylsp.setup {
-        handlers = handlers,
-        capabilities = capabilities,
-        on_attach = on_attach,
-        settings = {
-          pylsp = {
-            plugins = {
-              pylsp_black = { enabled = true },
-              isort = { enabled = true, profile = 'black' },
-              autopep8 = { enabled = false },
-              pycodestyle = { enabled = false },
-              -- jedi = {
-              --   environment = '/Users/storcivia/code/etl-pipelines',
-              -- },
-            },
-          },
-        },
-        filetypes = (servers['pylsp'] or {}).filetypes,
-      }
-    else
-      require('lspconfig')[server_name].setup {
-        handlers = handlers,
-        capabilities = capabilities,
-        on_attach = on_attach,
-        settings = servers[server_name],
-        filetypes = (servers[server_name] or {}).filetypes,
-      }
-    end
-  end,
-}
