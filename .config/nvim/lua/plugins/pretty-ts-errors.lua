e980d569fb0cfc04b9e3ce751d1591ad1bc25304
saltor <storcivia@fastgrowingtrees.com>
2025-11-23 16:59:09 -0500

    Add beads for agentic issue tracking

---
+++ .config/nvim/lua/plugins/pretty-ts-errors.lua
@@ -0,0 +1,10 @@
+return {
+  -- {
+  --   'youyoumu/pretty-ts-errors.nvim',
+  --   opts = {
+  --     executable = 'pretty-ts-errors-markdown',
+  --     auto_open = true,
+  --     wrap = true,
+  --   },
+  -- },
+}
