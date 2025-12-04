e5da8a3779a3a411d9cf7a3208c4578f1c387be0
saltor <storcivia@fastgrowingtrees.com>
2025-12-04 15:23:47 -0500

    Proof of concept usage of bd with opencode

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -32,3 +32,6 @@ end)
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 require 'config/lazy'
+
+-- Setup daily-quote plugin
+require('daily-quote').setup()
