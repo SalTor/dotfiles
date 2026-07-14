0b457e001d75ba606a161a8e4b7166866a7d5008
saltor <storcivia@fastgrowingtrees.com>
2026-07-14 11:45:10 -0400

    feat(nvim): enable leap.nvim with explicit s/S mappings
    Uncomment the leap.nvim spec and source it from Codeberg
    (codeberg.org/andyg/leap.nvim) rather than the GitHub repo. Bind s in
    normal/visual/operator-pending to <Plug>(leap) and S to
    <Plug>(leap-from-window) instead of calling add_default_mappings(), so
    leap only claims the keys chosen here.

--- .config/nvim/lua/plugins/keymaps.lua
+++ .config/nvim/lua/plugins/keymaps.lua
@@ -56,10 +56,12 @@ return {
       cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
     end,
   },
-  -- {
-  --   'ggandor/leap.nvim',
-  --   config = function()
-  --     require('leap').add_default_mappings()
-  --   end,
-  -- },
+  {
+    'https://codeberg.org/andyg/leap.nvim',
+    config = function()
+      -- require('leap').add_default_mappings()
+      vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
+      vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
+    end,
+  },
 }
